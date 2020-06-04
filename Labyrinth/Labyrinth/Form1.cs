a70dd00bd12237d895ddf91dde2de178f96da323
Totsamiy4el <56583623+Totsamiy4el@users.noreply.github.com>
2020-06-04 22:48:40 +0300

    Add files via upload

---
+++ Labyrinth/Labyrinth/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Labyrinth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        private void label71_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(this.Location.X + 55, this.Location.Y + 60); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(this.Location.X + 55, this.Location.Y + 60);
+        }
+
+     
+        private void labelFinish_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("З ПЕРЕМОГОЮ ВАС!!!","МОЇ ВІТАННЯ!!!",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void label39_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
